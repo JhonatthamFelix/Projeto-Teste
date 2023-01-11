58ee085aba249fd0c8e87adf0be850cfcf063cdd
Snail Mail <>
2023-01-11 13:21:20 -0300

    Projeto Teste

---
+++ CosoleAppBank/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] names = { "Carlos Amiro", "Cesar Trali" };
+
+        for (int i = 0; 1 < 2; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+    }
+}
